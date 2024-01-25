66fbabe363e387a12431ce4978f755f04f1f374e
g4ss4n <g4ss4n@gmail.com>
2024-01-25 20:43:08 +0200

    add-education

--- src/components/Education/app.tsx
+++ src/components/Education/app.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import './app.css'; // Import the CSS file for the education section
 
 const Education: React.FC = () => (
   <section id="education">
-    <h2>Education</h2>
-    {/* Display your education details here */}
+    <h2>Education and Courses</h2>
+    <div className="education-card">
+      <h4>Computer Science BSc.</h4>
+      <p className="institution">University Of Haifa</p>
+      <p className="date">Oct 2019 - Oct 2024 | Present</p>
+    </div>
+    <div className="education-card">
+      <h4>Full Stack Web Development Bootcamp</h4>
+      <p className="institution">Kav Mashve</p>
+      <p className="date">July 2019 - Oct 2019</p>
+    </div>
   </section>
 );
 
